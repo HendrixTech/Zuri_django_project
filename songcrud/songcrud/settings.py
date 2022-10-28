315a0b6ae25b10c44fb21db51989adc6dcefd847
HendrixTech <hendrixobuks@gmail.com>
2022-10-28 16:33:49 +0100

    Week_5 Django CRUD(Working with Models and attributes

--- songcrud/songcrud/settings.py
+++ songcrud/songcrud/settings.py
@@ -37,8 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
-    #added an application created by me
     'musicapp',
 ]
 
